4270d736fe9e1f6150c96872159f552c45f9f016
Ludovic Bonivento <ludovic.bonivento@epfl.ch>
2021-11-19 16:54:13 +0100

    debug error in isa.php

--- include/isa.php
+++ include/isa.php
@@ -4,38 +4,39 @@
 
 Class IsaRequest {
 
-    protected $baseUrl = null;
-    protected $headers = null;
-    protected $Http = null;
-    protected $options = null;
-    protected $config = null;
-    protected $type = null;
-    protected $args = null;
+	protected $baseUrl = null;
+	protected $headers = null;
+	protected $Http = null;
+	protected $options = null;
+	protected $config = null;
+	protected $type = null;
+	protected $args = null;
 
-    public function __construct() {
-        $this->baseUrl = 'https://isa.epfl.ch/services/';
-        $this->args = array('headers' => array('Accept' => 'application/xml', 'Accept-Charset' => 'UTF-8'));
-    }
+	public function __construct() {
+		$this->baseUrl = 'https://isa.epfl.ch/services/';
+		$this->args = array('headers' => array('Accept' => 'application/xml', 'Accept-Charset' => 'UTF-8'));
+	}
 
-    public function executeRequest($url) {
+	public function executeRequest($url) {
 
-        $data = null;
-        $url = $this->baseUrl . $url;
+		$data = null;
+		$url = $this->baseUrl . $url;
 
-        $request = wp_remote_get($url,$this->args);//new HTTP_Request2($url);
+		$request = wp_remote_get($url,$this->args);//new HTTP_Request2($url);
 
-        if( is_wp_error( $request ) ) {
-          echo "error ISARequest !";
-          return false;
-    		}
+		if( is_wp_error( $request ) ) {
+			echo "error ISARequest !";
+			return false;
+		}
 
-        $data = wp_remote_retrieve_body( $request );//t->getBody();
+		$data = wp_remote_retrieve_body( $request );//t->getBody();
 
-        if (empty($data)){
-            return null;
-    		}else{
-            return $data;
-    }
+		if (empty($data)){
+			return null;
+		}else{
+			return $data;
+		}
+	}
 }
 
 ?>
